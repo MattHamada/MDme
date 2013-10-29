56f3f02c5b294420156c2db34ba51b86625d0bf6
MattHamada <ermacaz@gmail.com>
2013-10-29 11:38:23 -0700

    renamed users to patients

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ MDme::Application.routes.draw do
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
 
+  resources :patients
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
