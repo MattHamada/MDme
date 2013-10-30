f3427b5c805504cd2af8341dda428f74885751c7
MattHamada <ermacaz@gmail.com>
2013-10-29 20:17:48 -0700

    Lowered bcrypt cost to speed tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ MDme::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  #speed up tests by lowering bcrypt encryption cost
+  ActiveModel::SecurePassword.min_cost = true
 end
