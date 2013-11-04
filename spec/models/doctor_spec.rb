05122322462fbcdf7d231bfdf3bb185e8f8e5f0d
MattHamada <ermacaz@gmail.com>
2013-11-04 11:35:32 -0700

    updated doctor validations and remember_token settings

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Doctor do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @doctor = Doctor.new(first_name: "Example", last_name: 'Doctor', email: "user@example.com", password: 'foobar', password_confirmation: 'foobar') }
+
+  subject { @doctor }
+
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:remember_token) }
+
+
+
 end
