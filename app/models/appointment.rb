1a6ff8ecb2518b6e953d153f64df9df451111047
MattHamada <ermacaz@gmail.com>
2013-11-04 12:08:33 -0700

    added appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,9 @@
 class Appointment < ActiveRecord::Base
 
+  validates :appointment_time, presence: true, uniqueness: true
+  validates :doctor_id, presence: true
+  validates :patient_id, presence:true
+
   belongs_to :doctor
   belongs_to :patient
 
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe Appointment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @doctor = Doctor.new(first_name: "Example", last_name: 'Doctor', email: "user@example.com", password: 'foobar', password_confirmation: 'foobar')
+    @patient = Patient.new(first_name: "Example", last_name: 'patient', email: "user@example.com", password: 'foobar', password_confirmation: 'foobar')
+    @appointment = Appointment.new(doctor_id: @doctor.id, patient_id: @patient.id, appointment_time: "2013-12-20 10:30:00")
+  end
+
+  subject { @appointment }
+  it { should respond_to(:doctor_id) }
+  it { should respond_to(:patient_id) }
+  it { should respond_to(:appointment_time) }
+
+  describe 'when doctor_id is missing' do
+    before { @appointment.doctor_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'when patient_id is missing' do
+    before { @appointment.patient_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'when appointment_time is missing' do
+    before { @appointment.appointment_time = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'doctors should not have more than one appointment at same time' do
+    before do
+      @appointment2 = @appointment.clone
+      @appointment2.save
+    end
+
+    it { should_not be_valid }
+  end
 end
+
