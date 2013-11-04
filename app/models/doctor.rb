fa2145992cad2fdaf95855b9ce93e56ede829c2e
MattHamada <ermacaz@gmail.com>
2013-11-04 11:01:39 -0700

    updated doctor validations and remember_token settings

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,24 @@
 class Doctor < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :first_name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
+  validates :last_name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  validates :password, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
 
   has_many :appointments
   has_many :patients
 
   has_secure_password
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = Patient.encrypt(Patient.new_remember_token)
+    end
+
 end
