0b2403d9995259664153b55fc0465b3f2cae4e55
MattHamada <ermacaz@gmail.com>
2013-11-05 14:40:07 -0700

    started design of admin show page and sidebar

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,9 +1,24 @@
 class AdminsController < ApplicationController
-  def login
+
+  before_filter :require_admin_login, :except => :signin
+
+
+  def signin
 
   end
+
+  def index
+  end
+
 
   def show
-
+    @appointments = Appointment.today.order('appointment_time ASC').all
   end
+
+  private
+    def require_admin_login
+      unless admin_signed_in?
+        redirect_to root_path
+      end
+    end
 end
