f870f529370c722e9b2ee0639005d5213089b205
MattHamada <ermacaz@gmail.com>
2013-11-04 17:27:55 -0700

    removed name uniqueness validator

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :first_name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
-  validates :last_name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
+  validates :first_name, presence: true, length: {maximum: 50}
+  validates :last_name, presence: true, length: {maximum: 50}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
   validates :password, length: { minimum: 6 }
 
