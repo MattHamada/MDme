c8175574fa8a29858f488c4fbf4cd6ab3acd8fb4
MattHamada <ermacaz@gmail.com>
2013-11-10 10:25:52 -0700

    creating new appointments working

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,8 +7,25 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-
+    year   = params[:date][:year].to_i
+    month  = params[:date][:month].to_i
+    day    = params[:date][:day].to_i
+    hour   = params[:date][:hour].to_i
+    minute = params[:date][:minute].to_i
+    date = DateTime.new(year, month, day, hour, minute)
+    @appointment = Appointment.new(doctor_id: params[:doctor][:doctor_id],
+                                   patient_id: params[:patient][:patient_id],
+                                   appointment_time: date,
+                                   description: params[:appointment][:description])
+    if @appointment.save
+      flash[:success] = "Appointment created"
+      redirect_to appointments_path
+    else
+      flash[:danger] = "Error creating appointment"
+      redirect_to new_appointment_url
+    end
   end
+
 
   def edit
 
