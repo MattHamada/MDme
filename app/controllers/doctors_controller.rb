1d0b5bf76781b6d37679ef395de722a40e76dde5
MattHamada <ermacaz@gmail.com>
2013-11-18 18:38:37 -0700

    admin doctor-new/edit methods and pages

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -17,4 +17,40 @@ class DoctorsController < ApplicationController
   def new
     @doctor = Doctor.new
   end
+
+  def create
+    p = doctor_params
+    p[:password] = 'temppass'
+    p[:password_confirmation]  = 'temppass'
+    @doctor = Doctor.new(p)
+
+    if @doctor.save
+      flash[:success] = 'Doctor Successfully Created.'
+      redirect_to admins_path
+    else
+      flash.now[:danger] = 'Invalid Parameters Entered'
+      render 'new'
+    end
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    @doctor.is_admin_applying_update = true if request.subdomain == 'admin'
+    @doctor.attributes = doctor_params
+    if @doctor.save
+      flash[:success] = "Doctor Successfully Updated"
+      redirect_to admins_path
+    else
+      flash.now[:danger] = 'Invalid Parameters Entered'
+      render 'new'
+    end
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :email, :department_id, :password, :password_confirmation)
+  end
 end
