3366e15d397e4f05721736d54c418ff7534f6a73
MattHamada <mattahamada@gmail.com>
2015-01-04 04:53:12 -0500

    updated capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 require 'subdomains'
 #require 'factory_girl_rails'
 
