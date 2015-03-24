8f73595dbde0c071cd589f9666031021ff709783
MattHamada <mattahamada@gmail.com>
2015-03-24 09:45:17 -0700

    login via angular jwt working for patients

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,48 @@
+angular.module('mdme')
+    .config(['$stateProvider', '$urlRouterProvider', 'AccessLevels', function($stateProvider, $urlRouterProvider, AccessLevels) {
+
+    $stateProvider
+      .state('anon', {
+        abstract: true,
+        template: '<ui-view/>',
+        data: {
+          access: AccessLevels.anon
+        }
+      })
+      .state('anon.home', {
+        url: '/',
+        templateUrl: 'static_pages/index.html',
+        controller: 'StaticPagesController'
+      })
+      .state('anon.about', {
+        url: '/about',
+        templateUrl: 'static_pages/about.html',
+        controller: 'StaticPagesController'
+      })
+      .state('anon.contact', {
+        url: '/contact',
+        templateUrl: 'static_pages/contact.html',
+        controller: 'ContactController'
+      })
+      .state('anon.signin', {
+        url: '/signin',
+        templateUrl: 'static_pages/signin.html',
+        controller: 'SessionsController'
+      });
+
+    $stateProvider
+      .state('user', {
+        abstract: true,
+        template: '<ui-view/>',
+        data: {
+          access: AccessLevels.user
+        }
+      })
+      .state('user.home', {
+        url: '/patients',
+        templateUrl: 'patients/show.html',
+        controller:  'PatientsController'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }]);
